08b0d5ebe7807f94dfb84c5a6651b4acca032566
vistrox <supawich.p@kkumail.com>
2025-02-22 15:17:18 +0700

    login and database

---
+++ website/models.py
@@ -0,0 +1,18 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # in ForeignKey() reference the parent class "User" then the field of the parent class "id"
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    userName = db.Column(db.String(150))
+    notes = db.relationship('Note')
+    # in relationship() reference the child
